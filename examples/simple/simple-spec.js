a44cd1af2fbc759aefe7607215aa674de04679ff
fool2fish <fool2fish@gmail.com>
2013-05-08 15:08:52 +0800

    A: simple example

---
+++ examples/simple/simple-spec.js
@@ -0,0 +1,18 @@
+define(function(require) {
+
+    var $ = require('$')
+    var expect = require('expect')
+
+    describe('Test Suit', function() {
+
+        it('Test Unit', function() {
+            expect('assertion').to.be.a('string')
+        })
+
+        describe('Sub Test Suit', function() {
+            it('Sub Test Unit', function() {
+                expect('sub assertion').to.be.a('string')
+            })
+        })
+    })
+});
