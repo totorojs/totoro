9e1722d8099765113e7370f3ee3d5bbffad0dd5d
kangpangpang <kangpangpang@gmail.com>
2013-07-11 23:12:34 +0800

    A: add tcp-over-json testcase

--- examples/performance/client_server.js
+++ examples/performance/client_server.js
@@ -5,6 +5,8 @@ var path = require('path')
 var http = require('http')
 var common = require('totoro-common')
 var io = require('socket.io-client')
+var jsonOverTCP = require('json-over-tcp')
+
 
 process.chdir(process.cwd())
 
@@ -12,7 +14,7 @@ var app = express()
 var cfg = {
     clientHost: common.getExternalIpAddress(),
     clientPort: 9998,
-    serverHost: '10.15.52.87',
+    serverHost: common.getExternalIpAddress(),
     serverPort: 9996
 }
 
@@ -23,32 +25,9 @@ app.listen(cfg.clientPort, cfg.clientHost, function() {
             'http://' + cfg.serverHost + ':' + cfg.serverPort + '/proxy')
 
     socket.on('getInfo', function (info) {
-        var opts = {
-            hostname: cfg.clientHost,
-            port: cfg.clientPort,
-            path: '/datas/' + info,
-            method: 'GET'
-        }
-        http.request(opts, function(res) {
-            var buffer = new Buffer(parseInt(res.headers['content-length'], 10))
-            var offset = 0
-
-            res.on('data', function(data) {
-                data.copy(buffer, offset)
-                offset += data.length
-            })
-
-            res.on('end', function() {
-                socket.emit('proxyData', {
-                    path: info,
-                    statusCode: res.statusCode,
-                    headers: res.headers,
-                    body: buffer
-                })
-            })
-        }).on('error', function(err) {
-            console.info('error', err)
-        }).end()
+        getInfo(cfg, info, function(info) {
+            socket.emit('proxyData', info)
+        })
     })
 
     var socket2 = io.connect(
@@ -60,4 +39,59 @@ app.listen(cfg.clientPort, cfg.clientHost, function() {
             port: cfg.clientPort
         })
     })
+
+    var socket3 = io.connect(
+            'http://' + cfg.serverHost + ':' + cfg.serverPort + '/tcp')
+
+    socket3.on('begin', function() {
+        socket3.emit('begin', {
+            hostname: cfg.clientHost,
+            port: 9997
+        })
+    })
+
+    var server = jsonOverTCP.createServer({
+        port: 9997,
+        host: cfg.clientHost
+    });
+    server.listen(9997);
+
+    server.on('connection', function(socket) {
+        socket.on('data', function(data) {
+
+            getInfo(cfg, data.path, function(info) {
+                socket.write(info)
+            })
+        })
+    })
 })
+
+function getInfo(cfg, p, cb) {
+    var opts = {
+        hostname: cfg.clientHost,
+        port: cfg.clientPort,
+        path: '/datas/' + p,
+        method: 'GET'
+    }
+
+    http.request(opts, function(res) {
+        var buffer = new Buffer(parseInt(res.headers['content-length'], 10))
+        var offset = 0
+
+        res.on('data', function(data) {
+            data.copy(buffer, offset)
+            offset += data.length
+        })
+
+        res.on('end', function() {
+            cb({
+                path: p,
+                statusCode: res.statusCode,
+                headers: res.headers,
+                body: buffer
+            })
+        })
+    }).on('error', function(err) {
+        console.info('error', err)
+    }).end()
+}
