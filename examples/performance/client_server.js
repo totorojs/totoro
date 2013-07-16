2fe073ae5f2a0ea86c5d3eb5a1c21fada4028300
kangpangpang <kangpangpang@gmail.com>
2013-07-16 11:18:22 +0800

    U: update

--- examples/performance/client_server.js
+++ examples/performance/client_server.js
@@ -6,6 +6,7 @@ var http = require('http')
 var common = require('totoro-common')
 var io = require('socket.io-client')
 var jsonOverTCP = require('json-over-tcp')
+var net = require('net')
 
 
 process.chdir(process.cwd())
@@ -57,13 +58,47 @@ app.listen(cfg.clientPort, cfg.clientHost, function() {
     server.listen(9997);
 
     server.on('connection', function(socket) {
+        console.info('coonect000')
         socket.on('data', function(data) {
+            //console.info('data------>', data)
 
             getInfo(cfg, data.path, function(info) {
+                //console.info('---------->info---->', info.path)
                 socket.write(info)
             })
         })
     })
+
+    var socket4 = io.connect(
+            'http://' + cfg.serverHost + ':' + cfg.serverPort + '/net')
+
+    socket4.on('begin', function() {
+        console.info('---------net socket begin--')
+        socket4.emit('begin', {
+            hostname: cfg.clientHost,
+            port: 9996
+        })
+    })
+
+    var netServer = net.createServer(function(c) { //'connection' listener
+        console.log('net server connected')
+
+        c.on('data', function(buf) {
+            console.info('net data----->', buf.length, buf + '')
+            getInfo(cfg, buf + '', function(info) {
+                console.info('--------net-->info---->', info.body)
+                socket.write(info.body + '\r\n')
+            })
+        })
+
+        c.on('end', function() {
+          console.log('server disconnected')
+        })
+        c.pipe(c)
+    })
+
+    netServer.listen(9996)
+
 })
 
 function getInfo(cfg, p, cb) {
@@ -73,7 +108,6 @@ function getInfo(cfg, p, cb) {
         path: '/datas/' + p,
         method: 'GET'
     }
-
     http.request(opts, function(res) {
         var buffer = new Buffer(parseInt(res.headers['content-length'], 10))
         var offset = 0
