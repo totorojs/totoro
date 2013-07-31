8f377afab35e42b803b4181d64a0cc83da3773cb
fool2fish <fool2fish@gmail.com>
2013-07-31 13:45:56 +0800

    U: update examples

--- examples/test-fail/tests/example-spec.js
+++ examples/test-fail/tests/example-spec.js
@@ -8,8 +8,10 @@ define(function(require) {
             expect('assertion').to.be.a('string')
         })
 
-        // NOTE there is an error!
-        var val = undef.attr
-
+        describe('Sub Test Suit', function() {
+            it('Sub Test Unit', function() {
+                expect('sub assertion').to.be.a('number')
+            })
+        })
     })
 })
