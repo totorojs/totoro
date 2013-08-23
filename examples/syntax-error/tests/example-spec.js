7aa633bee1eac4bc178158d3c6f1fd47354c8114
hui.kang <kangpangpang@gmail.com>
2013-08-23 11:26:17 +0800

    U: update syntax test case

--- examples/syntax-error/tests/example-spec.js
+++ examples/syntax-error/tests/example-spec.js
@@ -2,9 +2,12 @@ define(function(require) {
 
     var expect = require('expect')
 
+    var test = require('../src/test')
+
     describe('Test Suit', function() {
 
         it('Test Unit', function() {
+            expect(test.say()).to.be('hello')
             expect('assertion').to.be.a('string')
         })
 
