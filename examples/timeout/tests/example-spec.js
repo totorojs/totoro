4a164d9f121fc7be42bee314608157a65d244e8f
fool2fish <fool2fish@gmail.com>
2013-11-01 22:13:11 +0800

    Reduce timeout setting.

--- examples/timeout/tests/example-spec.js
+++ examples/timeout/tests/example-spec.js
@@ -1,12 +1,12 @@
 define(function(require) {
     var expect = require('expect')
     describe('A Test Suite', function() {
-        this.timeout(300000)
+        this.timeout(30000)
         it('A Test Case', function(done) {
             setTimeout(function() {
                 expect('1').to.eql(1)
                 done()
-            }, 100000)
+            }, 10000)
         })
     })
 })
