37fe13e243edf9ea8313da4a87df1d249c9ca719
fool2fish <fool2fish@gmail.com>
2014-02-27 15:17:00 +0800

    Update.

--- examples/simple/src/example.js
+++ examples/simple/src/example.js
@@ -1,5 +1,5 @@
 define(function(require, exports, module) {
-    console.log('I am a  log message.')
+    console.log('I am a log message.')
     module.exports = function() {
         return 'A simple sample.'
     }
